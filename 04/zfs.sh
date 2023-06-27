7128145d7bd443541e7f9eb6517da78b8e99efce
root <vovka23@inbox.ru>
2023-06-27 18:40:32 +0300

    04

---
+++ 04/zfs.sh
@@ -0,0 +1,13 @@
+#bash
+sudo -i
+zpool create otus1 mirror /dev/sdb /dev/sdc
+zpool create otus2 mirror /dev/sdd /dev/sde
+zpool create otus3 mirror /dev/sdf /dev/sdg
+zpool create otus4 mirror /dev/sdh /dev/sdi
+zpool status
+zpool list
+zfs set compression=lzjb otus1
+zfs set compression=lz4 otus2
+zfs set compression=gzip-9 otus3
+zfs set compression=zle otus4
+
